022ef0887fd600cffac86cf00b2619350f8b70ce
twinbird <ixa2063@gmail.com>
2016-10-16 01:13:04 +0900

    ちょこっとテスト追加

--- test.sh
+++ test.sh
@@ -7,3 +7,16 @@ if [[ $test1 != "2" ]]; then
 	exit 1
 fi
 
+# test2
+test2=`echo "(print 1)" | ./pure -e`
+if [[ $test2 != "1" ]]; then
+	echo -n "test2 failed.Expect:1, Actual:${test2}"
+	exit 1
+fi
+
+# test3
+test3=`echo '(print "hello")' | ./pure -e`
+if [[ $test3 != '"hello"' ]]; then
+	echo -n "test3 failed.Expect:'"hello"', Actual:${test3}"
+	exit 1
+fi
